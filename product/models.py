bc0702256985623954e6b8849e821f3eea36b72d
afurkanyegin <abdullahfurkanyegin@outlook.com>
2020-06-13 18:40:18 +0300

    first commit

---
+++ product/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    STATUS=(('True','Evet'),('False','Hayır'),)
+    parent =models.ForeignKey('self',blank=True,null=True,related_name='children',on_delete=models.CASCADE)
+    title=models.CharField(max_length=150)
+    keywords=models.CharField(max_length=255)
+    description=models.CharField(max_length=255)
+    image=models.ImageField(blank=True,upload_to='images/')
+    status=models.CharField(max_length=10,choices=STATUS)
+    slug=models.SlugField()
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    STATUS=(('True','Evet'),('False','Hayır'),)
+
+    category =models.ForeignKey(Category,on_delete=models.CASCADE)
+    title=models.CharField(max_length=150)
+    keywords=models.CharField(max_length=255)
+    description=models.CharField(max_length=255)
+    image=models.ImageField(blank=True,upload_to='images/')
+    price=models.FloatField()
+    amount=models.IntegerField()
+    minamount=models.IntegerField()
+    detail=models.TextField()
+    status=models.CharField(max_length=10,choices=STATUS)
+    slug=models.SlugField()
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
